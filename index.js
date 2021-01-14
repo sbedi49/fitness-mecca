beb0bcc7c407c498a7c4dbaa537c4a13c383805b
Sarab Bedi <sarabsbedi@gmail.com>
2021-01-14 18:04:11 -0500

    Added hamburger menu

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const faqCard = document.querySelectorAll(".faq-card");
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("resp_nav")
 
 faqCard.forEach(card => {
     card.addEventListener("click",()=>{
         const text = card.lastElementChild
         text.classList.toggle("active")
     })
+})
+
+toggle.addEventListener("click",()=>{
+    nav.classList.toggle("active");
 })
 
 
